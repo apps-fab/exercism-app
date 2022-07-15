2693a8260512d127057915fb0c3b569a87dd9781
Kirk Agbenyegah <savekirk@gmail.com>
2022-07-15 19:06:34 +0300

    Add swiftUI skeleton project

---
+++ Exercism/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Exercism
+//
+//  Created by Kirk Agbenyegah on 04/07/2022.
+//
+
+import SwiftUI
+import ExercismSwift
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exercism/ExercismApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExercismApp.swift
+//  Exercism
+//
+//  Created by Kirk Agbenyegah on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExercismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
