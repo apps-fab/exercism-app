5b8381940b35751f5711a471e94fae6e275b595c
angiemugo <angela@basecamp.com>
2022-10-06 13:59:20 +0300

    add dashboard

--- Exercism/DashBoard.swift
+++ Exercism/DashBoard.swift
@@ -7,9 +7,45 @@
 
 import SwiftUI
 
+enum ItemType: String {
+    case recentTrack = "Tracks"
+    case profile = "Profile Items"
+}
+
+struct DashboardItem: Identifiable {
+    let name: String
+    let image = "folder.fill"
+    let id = UUID()
+}
+
+struct DashboardSections: Identifiable {
+    let type: ItemType
+    let items: [DashboardItem]
+    let id = UUID()
+}
+
 struct DashBoard: View {
+
+    private var itemNames = [DashboardSections(type: .profile, items: [DashboardItem(name: "All tracks"), DashboardItem(name: "Solutions"),
+                                                                       DashboardItem(name: "Badges")]),
+                             DashboardSections(type: .recentTrack, items:  [DashboardItem(name: "Elixir"),
+                                                                            DashboardItem(name: "Rust")])]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List() {
+            ForEach(itemNames) { name in
+                Section(header: Text(name.type.rawValue)) {
+                    ForEach(name.items) { name in
+                        HStack {
+                            Image(systemName: name.image)
+                                .renderingMode(.template)
+                                .foregroundColor(.blue)
+                            Text(name.name)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
