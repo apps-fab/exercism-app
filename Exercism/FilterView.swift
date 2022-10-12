e7196baeab6a7b9d83968a3826feed4dffe41adf
angiemugo <angela@basecamp.com>
2022-10-12 14:08:43 +0300

    style filter view

--- Exercism/FilterView.swift
+++ Exercism/FilterView.swift
@@ -15,20 +15,35 @@ struct FilterView: View {
                 TextField("", text: .constant("Search language filters"))
                     .textFieldStyle(.plain)
             }.padding()
-                .background(RoundedRectangle(cornerRadius: 14))
+                .background(RoundedRectangle(cornerRadius: 14)
+                    .fill(.black))
 
             Button {
                 showFilters()
             } label: {
-                Label("Show Filters", systemImage: "line.3.horizontal.decrease.circle").frame(minHeight: 0, maxHeight: .infinity)
-            }
-            .padding(.horizontal, 10)
+                Label("Show Filters", systemImage: "line.3.horizontal.decrease.circle")
+                    .padding()
+                    .frame(minHeight: 0, maxHeight: .infinity)
+                    .background(.black)
+            }.buttonStyle(.plain)
+                .cornerRadius(10)
+                .padding(.horizontal, 10)
+
             Text("Showing all 26 tracks")
+                .padding()
+                .frame(minHeight: 0, maxHeight: .infinity)
+                .background(RoundedRectangle(cornerRadius: 14)
+                .fill(.black))
             Button {
                 showFilters()
             } label: {
-                Label("Sort by last touched", systemImage: "chevron.down").frame(minHeight: 0, maxHeight: .infinity)
-            }.padding()
+                Label("Sort by last touched", systemImage: "chevron.down")
+                    .padding()
+                    .frame(minHeight: 0, maxHeight: .infinity)
+                    .background(.black)
+            }.buttonStyle(.plain)
+                .cornerRadius(10)
+                .padding(.horizontal, 10)
         }
     }
 
