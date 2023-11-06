7f9cbabe93d9a68b5152aaa3447147c78c81cb1e
angiemugo <angela@basecamp.com>
2023-11-06 13:52:00 +0300

    remove all unwanter files - .vscode, fastlane

--- fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "com.apps-fab.exercism.Exercism" } // The bundle identifier of your app
-var appleID: String { return "mugoangela@gmail.com" } // Your Apple Developer Portal username
-
-var itcTeam: String? { return "119401781" } // App Store Connect Team ID
-var teamID: String { return "S82GZQ59NS" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
