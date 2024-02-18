1ba4e4a5fe2766f8c86b6e613f6cdb9f1931352f
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-02-18 11:05:36 +0200

    Create custom picker

--- Exercism/ExerciseList/CustomPicker.swift
+++ Exercism/ExerciseList/CustomPicker.swift
@@ -7,17 +7,38 @@
 
 import SwiftUI
 
-struct CustomPicker<Content: View, Selection: Identifiable>: View {
-    @State var selected: Selection
-    @ViewBuilder var content: () -> Content
+struct CustomPicker<Content: View, Selection: Identifiable&Equatable>: View {
+    @Binding var selected: Selection
+    let items: [Selection]
+    @ViewBuilder var labelContent: (Selection) -> Content
     
     var body: some View {
-        content()
+        HStack {
+            ForEach(items) { item in
+                labelContent(item)
+                    .fontWeight(
+                        item == selected ? .semibold : .regular)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.9)
+                    .padding()
+                    .frame(height: 34)
+                    .roundEdges(backgroundColor: item == selected ? Color.exercismPurple.opacity(0.1) : .clear, lineColor: .clear, cornerRadius: 25)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        selected = item
+                    }
+                    .frame(maxWidth: .infinity)
+            }
+        }
     }
 }
 
+extension CustomPicker where Content == Text {}
+
 #Preview {
-    CustomPicker(selected: ExerciseCategory.AllExercises) {
-        Text("Some")
-    }
+    CustomPicker(
+        selected: .constant(ExerciseCategory.AllExercises),
+        items: ExerciseCategory.allCases) {
+            Text("Some \($0.rawValue)")
+        }
 }
