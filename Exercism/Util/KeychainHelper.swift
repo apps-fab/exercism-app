96490445541431c0a7abafe7e72b8a9daa4c5f1b
angiemugo <angela@basecamp.com>
2024-10-15 20:14:58 +0300

    use keychain access

--- Exercism/Util/KeychainHelper.swift
+++ Exercism/Util/KeychainHelper.swift
@@ -5,21 +5,39 @@
 //  Created by Angie Mugo on 29/09/2022.
 //
 
-@preconcurrency import KeychainSwift
+@preconcurrency import KeychainAccess
 
 final class ExercismKeychain: Sendable {
-   public static let shared = ExercismKeychain()
-    private let keychain = KeychainSwift()
+    public static let shared = ExercismKeychain()
+    private let keychain: Keychain
+
+    init() {
+        keychain = Keychain()
+            .synchronizable(true)
+    }
 
     func set(_ value: String, for key: String) {
-        keychain.set(value, forKey: key)
+        do {
+            try keychain.set(value, key: key)
+        } catch let error {
+            print("Error setting keychain item: \(error)")
+        }
     }
 
     func get(for key: String) -> String? {
-        keychain.get(key)
+        do {
+            return try keychain.get(key)
+        } catch {
+            print("Error setting keychain item: \(error)")
+            return nil
+        }
     }
 
     func removeItem(for key: String) {
-        keychain.delete(key)
+        do {
+            try keychain.remove(key)
+        } catch let error {
+            print("error: \(error)")
+        }
     }
 }
